394ecb45b930ae89f8833b894ce86f2082413d7e
007Will <renome@live.com>
2025-09-05 14:53:52 -0300

    Automações com outra base Docker

--- web/cypress/e2e/pre-cadastro.cy.js
+++ web/cypress/e2e/pre-cadastro.cy.js
@@ -1,17 +1,16 @@
-describe('Login', () => {
-  it('Deve locag com sucesso', () => {
+describe('Pré-Cadastro', () => {
+  it('Deve logar o pré-cadastro com sucesso', () => {
     cy.visit('/')
-    cy.get('header nav a[href=entrar]')
+
+    cy.get('header nav a[href=pre-cadastro]')
       .click()
 
     cy.get('form h2').should('be.visible').and('have.text', 'Seus dados')
 
-    cy.get('input[placeholder="Nome"]').type('Fernando papito')
+    cy.get('input[name="nome"]').type('Fernando papito')
 
-    cy.get('input[placeholder="E-mail"]').type('fernando@msn.com')   
+    cy.get('input[name="email"]').type('fernando@msn.com')   
 
-    cy.get('input[placeholder="Whatsapp"]').type('11999999999')
-    
     cy.contains('button[type=submit]', 'Continuar').click()
     
     cy.get('.user-name').should('be.visible').and('have.text', 'Fernando papito')
